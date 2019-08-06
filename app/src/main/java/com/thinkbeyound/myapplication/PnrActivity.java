5c2b9fcf9f768d3efb43ccae43fb430ad1f532a7
Harshavardhan <bharsha2007@gmail.com>
2019-08-06 17:45:01 +0200

    PNR Activity

---
+++ app/src/main/java/com/thinkbeyound/myapplication/PnrActivity.java
@@ -0,0 +1,14 @@
+package com.thinkbeyound.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PnrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pnr);
+    }
+}
