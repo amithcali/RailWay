71254f02483183fda4e3526e3ed89c553bc5b69b
Harshavardhan <bharsha2007@gmail.com>
2019-08-06 17:22:29 +0200

    Added Spot and PNR intents

--- app/src/main/java/com/thinkbeyound/myapplication/MainActivity.java
+++ app/src/main/java/com/thinkbeyound/myapplication/MainActivity.java
@@ -2,13 +2,38 @@ package com.thinkbeyound.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button spot,pnr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spot = (Button) findViewById(R.id.spot);
+        pnr = (Button) findViewById(R.id.pnr);
+
+        spot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent spotAct = new Intent(getApplicationContext(), Spotting.class);
+                startActivity(spotAct);
+            }
+        });
+
+
+        pnr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pnrAct = new Intent(getApplicationContext(), PnrActivity.class);
+                startActivity(pnrAct);
+            }
+        });
     }
 }
